0ca0fc7efd538b64f6ac074ba68aeb516fc64328
Saturn <franco.schirra2000@gmail.com>
2026-02-15 02:53:43 +0100

    is it gone now?

--- repak-gui/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Utility functions re-exports
- * Import from this file for convenience: import { formatFileSize, toTagArray } from './utils'
- */
-
-export { toTagArray } from './tags';
-export { detectHeroes, detectHeroesWithData } from './heroes';
-export { formatFileSize, normalizeModBaseName } from './format';
-export { getAdditionalCategories } from './mods';
